873e277ebb0e8e886b713a016aff319c0feefa2e
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-15 23:15:15 +0300

    Add login required functionality

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,8 @@
-from flask import redirect, render_template,url_for, flash
+from flask import redirect, render_template,url_for, flash, request
 from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm
 from blog.models import User, Post, Comment
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_required, login_user, current_user, logout_user
 
 
 blogs = [
@@ -53,10 +53,9 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            # next_page = request.args.get('next')
-            # return redirect(next_page) if next_page else redirect(url_for('home'))
+            next_page = request.args.get('next')
             flash('Login successful. Happy blogging', 'success')
-            return redirect('home')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email or password', 'danger')
     return render_template('login.html', title="login",form=form)
@@ -80,3 +79,8 @@ def logout():
     logout_user()
     flash('User logout successful','success')
     return redirect('login')
+
+@app.route("/profile")
+@login_required
+def profile():
+    return render_template('profile.html', title="profile")
