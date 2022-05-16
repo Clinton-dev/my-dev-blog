c0e9df909c0b01ff7e6fead20e2750bd6df28fac
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-16 21:26:27 +0300

    Add random quote functionality

--- blog/routes.py
+++ blog/routes.py
@@ -1,18 +1,29 @@
+from multiprocessing import reduction
 import os
+import json
+import requests
 import secrets
 from PIL import Image
-from flask import redirect, render_template,url_for, flash, request
+from flask import abort, redirect, render_template,url_for, flash, request
 from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm, UpdateProfileForm, PostForm
-from blog.models import User, Post, Comment
+from blog.models import User, Post, Comment, Quote
 from flask_login import login_required, login_user, current_user, logout_user
 
-
+def get_quote():
+    quote_url = 'http://quotes.stormconsultancy.co.uk/random.json'
+    req = requests.get(quote_url)
+    data = json.loads(req.content)
+    quote = Quote(data["quote"],data["author"])
+    return quote
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", blogs=blogs)
+    quote = get_quote()
+    print(quote)
+    blogs = Post.query.all()
+    return render_template("index.html", blogs=blogs, rand_quote=quote)
 
 
 @app.route("/login",methods=["POST","GET"])
@@ -92,4 +103,39 @@ def create_post():
         db.session.commit()
         flash('Blog post created successfully!', 'success')
         return redirect(url_for('home'))
-    return render_template("create_post.html", title="new blog post", form=form)
+    return render_template("create_post.html", title="new blog post", form=form, legend="Create a post")
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title,post=post)
+
+@app.route("/post/<int:post_id>/update", methods=["POST","GET"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Blog updated successfully!', 'success')
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data=post.content
+    return render_template("create_post.html", title="update post", form=form, legend="Update post")
+
+@app.route("/post/<int:post_id>/delete", methods=["POST",'GET'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+            abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Blog post deleted successfully!','success')
+    return redirect(url_for('home'))
