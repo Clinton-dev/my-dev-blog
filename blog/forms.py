a42f1dcae031bd187103b1720f5fdb1555dd3ca5
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-16 12:54:27 +0300

    Add update functionality to user profile page

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired,Length, Email, EqualTo,ValidationError
@@ -32,6 +33,7 @@ class UpdateProfileForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
     # password = PasswordField('Password', validators=[DataRequired()])
     # confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    picture = FileField('update profile picture',validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Update profile')
 
     def validate_username(self, username):
--- blog/routes.py
+++ blog/routes.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+from PIL import Image
 from flask import redirect, render_template,url_for, flash, request
 from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm, UpdateProfileForm
@@ -80,9 +83,33 @@ def logout():
     flash('User logout successful','success')
     return redirect('login')
 
-@app.route("/profile")
+def save_picture(form_pic):
+    random_hex = secrets.token_hex(8)
+    _f_name,f_ext = os.path.splitext(form_pic.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile-pic', picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_pic)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/profile", methods=["POST","GET"])
 @login_required
 def profile():
     form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Profile updated successfully!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     img_file = url_for('static', filename='profile-pic/' + current_user.image_file)
     return render_template('profile.html', title="profile", user_img=img_file, form=form)
